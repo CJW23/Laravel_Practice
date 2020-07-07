7d4b5638a962a783235da3eb7ec383ed220203d7
CJW23 <cjw7242@gmail.com>
2020-07-07 17:55:54 +0900

    글 작성 창 개선

--- Project/resources/views/tasks/create.blade.php
+++ Project/resources/views/tasks/create.blade.php
@@ -7,17 +7,25 @@ CREATE
 @section('content')
 <form action='/tasks' method='POST'>
     @csrf
+    <br>
     <div>
-        <input type="text" name="title" id="title" value="{{old('title') ? old('title'):''}}">
+        <input type="text" class="form-control" style="display : block; margin : 0 auto; width:600px; height:50px;" name="title" id="title"
+            value="{{old('title') ? old('title'):''}}" placeholder="Title">
         @error('title')
         {{$message}}
         @enderror
     </div>
+    <br>
     <div>
-        <textarea name="des" id="des">{{old('des') ? old('des'):''}}</textarea>
+        <textarea name="des" id="des" class="form-control"
+            style="display : block; margin : 0 auto; width:600px; height:500px;">{{old('des') ? old('des'):''}}</textarea>
         @error('des')
         {{$message}}
         @enderror
     </div>
-    <button>submit</button> 
+    <br>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" style="display : block; margin : 0 auto;">submit</button>
+
+    </div>
+</form>
 @endsection
